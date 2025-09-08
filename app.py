6bc75a6de825a18401e178305865bfd4251cbb5c
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-08 13:18:07 +0530

    Try creating new Database

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
 from flask import Flask, jsonify
-
-
+import sqlite3
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def return_json():
     return jsonify({
-  "message": "hello world"
-})
-
-@app.route('/expenses')
-def return_expenses():
-    return jsonify({
-        "fruits" : {
-            "apples" : 10,
-            "oranges" : 20,
-            "pears" : 30,
-            "bananas" : 40,
-            "grapes" : 50
-        },
-        "bills": {
-            "phone bill" : 500,
-            "electric bill" : 1080,
-            "water bill" : 1000
-        }
+        "message": "hello world"
     })
 
 
+@app.route('/expenses')
+def return_expenses():
+    conn = sqlite3.connect('expenses.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM expenses")
+    rows = cursor.fetchall()
+    data = []
+    for row in rows:
+        data.append({
+            "id": row[0],
+            "item": row[1],
+            "amount": row[2]
+        })
+    return jsonify(data)
 
 
-
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    try:
+        conn = sqlite3.connect('expenses.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO expenses (id, item, amount) VALUES (1, "electicity bill", 1080);
+        """)
+        conn.commit()
+        print("working")
+        return "Data added successfully!"
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
