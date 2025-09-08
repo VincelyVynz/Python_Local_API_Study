3c8299c0300971298197165304154521764e0c3b
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-08 11:20:21 +0530

    Add a new route and create a db

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+# Importing sqlite3 to work with databases and create a connection & database
+import sqlite3
+conn = sqlite3.connect("expenses.db")
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def return_json():
+    return jsonify({
+  "message": "hello world"
+})
+
+@app.route('/expenses')
+def return_expenses():
+    return jsonify({
+        "fruits" : {
+            "apples" : 10,
+            "oranges" : 20,
+            "pears" : 30,
+            "bananas" : 40,
+            "grapes" : 50
+        },
+        "bills": {
+            "phone bill" : 500,
+            "electric bill" : 1080,
+            "water bill" : 1000
+        }
+    })
+
+
+
+
+
 
 
 if __name__ == '__main__':
