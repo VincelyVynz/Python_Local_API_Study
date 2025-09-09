cbefcfea91e194089c1d8efe46b73a9bc9fd87dc
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-09 10:50:40 +0530

    Create products and stock app routes in app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def working():
+    return "Working"
+
+@app.route('/products')
+def get_products():
+    conn = sqlite3.connect('store.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products")
+    rows = cursor.fetchall()
+    products = []
+    for row in rows:
+        products.append({"id": row[0], "name": row[1], "price": row[2], "stock": row[3]})
+    conn.close()
+    return jsonify(products)
+
+@app.route('/stock')
+def get_stock():
+    conn = sqlite3.connect('store.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products")
+    rows = cursor.fetchall()
+    stock = []
+    for row in rows:
+        if row[3] > 30:
+            stock.append({"id": row[0], "name": row[1], "price": row[2], "stock": row[3]})
+    conn.close()
+    return jsonify(stock)
+
